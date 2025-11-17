5c7f67848dbaf824e86396cb4f7692fc5553347a
phamthanh2007 <thanhsdm@gmail.com>
2025-11-17 22:01:22 +0700

    Add input and output for subtraction in hieu.cpp

--- hieu.cpp
+++ hieu.cpp
@@ -1,3 +1,10 @@
-int main(){
+#include <iostream>
+
+int main() {
+    int a, b;
+    std::cin >> a;
+    std::cin >> b;
+    int hieu = a - b;
+    std::cout << hieu << std::endl;
     return 0;
 }
